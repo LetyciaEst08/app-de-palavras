dd07b3d23e85c0c6a501a6bfbdf02ab190dad63c
LetyciaEst08 <letyciaesteves@hotmail.com>
2023-08-29 21:51:04 -0300

    Implementação dos Cards. Alinhamentos de collumns e rows botão favoritos e uso do refator

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Chuvisco',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 34, 115)),
         ),
         home: MyHomePage(),
       ),
@@ -31,26 +31,95 @@ class MyAppState extends ChangeNotifier {
   void getNext(){
     current = WordPair.random();
     notifyListeners();
+ }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)){
+      favorites.remove(current);
+    }else {
+      favorites.add(current);
+    }
+    notifyListeners();
   }
+ 
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+  
+    IconData icon;
+    if (appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    }else {
+      icon = Icons.favorite_border;
+    }
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('BRAINSTORM!'),
-          Text(appState.current.asLowerCase),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('BRAINSTORM!'),
+            BigCard(pair: pair),
+            SizedBox(height: 12),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(
+                   width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();
+                  }, child: Text('Próximo'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            }, child: Text('Próximo'),
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      
+    );
+
+    return Card(
+      elevation: 7,
+      color: theme.colorScheme.secondary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style:style,
+          semanticsLabel: '${pair.first} ${pair.second}',
           ),
-        ],
       ),
     );
   }
