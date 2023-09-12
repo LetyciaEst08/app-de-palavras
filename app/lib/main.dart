5bb3e5c39e38233df46f03df503c4de919574573
LetyciaEst08 <letyciaesteves@hotmail.com>
2023-09-12 09:12:58 -0300

    update class myHomePage

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -48,9 +48,18 @@ class MyAppState extends ChangeNotifier {
  
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: Row(
         children:[
@@ -67,9 +76,11 @@ class MyHomePage extends StatelessWidget {
                     label: Text('Favorites'),
                     ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             ),
           ),
